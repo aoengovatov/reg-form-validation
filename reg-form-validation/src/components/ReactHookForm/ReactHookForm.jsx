8e033ffdf40733c05a30fe7c23c01dfd0130b9df
AlexEngo <a.o.engovatov@yandex.ru>
2023-11-12 22:19:46 +0400

    add styles ReactHookForm component

--- reg-form-validation/src/components/ReactHookForm/ReactHookForm.jsx
+++ reg-form-validation/src/components/ReactHookForm/ReactHookForm.jsx
@@ -3,7 +3,7 @@ import * as yup from 'yup';
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from 'react-hook-form';
 
-const fieldScheme = yup.object()
+const fieldSchema = yup.object()
     .shape({
         email: yup.string()
             .matches(/^[A-Z0-9_-]+@[A-Z0-9]+.[A-Z]{2,4}$/i, 'Некорректный email. ' + 
@@ -20,6 +20,53 @@ const fieldScheme = yup.object()
             .max(20, 'Некорректный пароль. Длина должна быть не больше 20 символов.'),       
     });
 
+const sendFormData = (formData) => {
+    console.log(formData);
+};    
+
 export const ReactHookForm = () => {
-    return <div>инициализация компонента ReactHookForm</div>;
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        defaultValues: {
+            email: '',
+            password: '',
+            password2: '',
+        },
+        resolver: yupResolver(fieldSchema),
+    });
+
+
+    return (
+        <div className={styles.container}>
+        <div className={styles.title}>Регистрация</div>
+        <form className={styles.form} onSubmit={handleSubmit(sendFormData)}>
+            <input 
+                className={styles.input} 
+                name='email' 
+                type='text' 
+                {...register('email')} />
+            <input 
+                className={styles.input} 
+                name='password' 
+                type='password' 
+                {...register('password')} />
+            <input 
+                className={styles.input} 
+                name='password2' 
+                type='password' 
+                {...register('password2')} />
+            
+            <button 
+                className={styles.button} 
+                type='submit' 
+                disabled='false'>
+                    зарегистрироваться
+            </button>
+        </form>
+        <div className={styles.error}>ошибка</div>
+      </div>
+    );
 }
